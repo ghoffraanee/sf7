b724894e252e3707f20f0a2e950c94b75c45689c
ghoffraanee <ghofranemessaoud05@gmail.com>
2025-10-07 13:25:20 +0100

    Initial project

---
+++ tests/Service/CalculatorTest.php
@@ -0,0 +1,13 @@
+<?php 
+namespace App\Tests\Service; 
+use App\Service\Calculator; 
+use PHPUnit\Framework\TestCase;
+class CalculatorTest extends TestCase 
+{ 
+    public function testAdd(): void 
+    { 
+        $calculator = new Calculator(); 
+        $result = $calculator->add(2, 3); 
+        $this->assertEquals(5, $result); 
+    } 
+} 
